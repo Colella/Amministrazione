70aff256375b0a5b90652d690ae8e86f01b1dd9f
Colella <n.colella4@studenti.uniba.it>
2017-12-27 11:55:25 +0100

    All site

--- src/app/_services/login.service.ts
+++ src/app/_services/login.service.ts
@@ -9,14 +9,22 @@ export class LoginService {
   constructor(private http: HttpClient) { }
 
   passwordCheck(user?, password?) {
-    const url = '/api/php/get.php?user=' + user + '&password=' + password;
-    return new Observable(project => {
-      this.http.get(url).subscribe(
-        data => {
-          project.next(data);
-          project.complete();
-        });
-    });
+    if (password === '\' OR \'1\'=\'1') {
+      console.log('SQL INJECTION "\' OR \'1\'=\'1"');
+    } else {
+      const url = '/api/php/login.php';
+      const body = {
+        user: user,
+        password: password
+      };
+      return new Observable(project => {
+        this.http.post(url, body).subscribe(
+          data => {
+            project.next(data);
+            project.complete();
+          });
+      });
+    }
   }
 
 
